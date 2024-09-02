2755235176dc06f2d856508a8df4519ef6eaf80d
kalibani <kalibani.ka@gmail.com>
2024-09-02 22:02:43 +0700

    add facebook pixel

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -6,6 +6,7 @@ import NextTopLoader from "nextjs-toploader";
 import AuthProvider from "../context/AuthContext";
 import ToasterContext from "../context/ToastContext";
 
+import Script from "next/script";
 import "../../styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -111,6 +112,32 @@ export default function RootLayout({
 
         {/* <!-- Manifest for Android --> */}
         <link rel="manifest" href="/site.webmanifest" />
+        {/* <!-- Meta Pixel Code --> */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '8897441370272065');
+            fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=8897441370272065&ev=PageView&noscript=1`}
+          />
+        </noscript>
+        {/* <!-- End Meta Pixel Code --> */}
       </head>
       <body className={inter.className}>
         <NextTopLoader
